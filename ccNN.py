b8d5acfd639f177bd7cb825a8abebcd56223f5fa
Elena-Gomez-Orihuel <70576417+Elena-Gomez-Orihuel@users.noreply.github.com>
2023-09-13 15:24:39 +0200

    Add files via upload

---
+++ ccNN.py
@@ -0,0 +1,381 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Assignment: Image recognition
+# - Alumno 1: Elena María Gómez Orihuel
+# - Alumno 2: Dante Adami Peracchia
+# - Alumno 3: Javier Montoya Gutiérrez de Ravé
+
+# 
+# The goals of the assignment are:
+# * Develop proficiency in using Tensorflow/Keras for training Neural Nets (NNs).
+# * Put into practice the acquired knowledge to optimize the parameters and architecture of a feedforward Neural Net (ffNN), in the context of an image recognition problem (28th Feb - 7th Mar).
+# * Put into practice NNs specially conceived for analysing images. Design and optimize the parameters of a Convolutional Neural Net (CNN) to deal with previous task (7th Mar - 14th Mar).
+# * Train popular architectures from scratch (e.g., GoogLeNet, VGG, ResNet, ...), and compare the results with the ones provided by their pre-trained versions using transfer learning (14th Mar - 23rd Mar)
+# 
+# Follow the link below to download the classification data set  â??xview_recognitionâ?: [https://drive.upm.es/s/UeDNT7JEYFkCVPP](https://drive.upm.es/s/UeDNT7JEYFkCVPP)
+
+# In[1]:
+
+
+import uuid
+import numpy as np
+
+class GenericObject:
+    """
+    Generic object data.
+    """
+    def __init__(self):
+        self.id = uuid.uuid4()
+        self.bb = (-1, -1, -1, -1)
+        self.category= -1
+        self.score = -1
+
+class GenericImage:
+    """
+    Generic image data.
+    """
+    def __init__(self, filename):
+        self.filename = filename
+        self.tile = np.array([-1, -1, -1, -1])  # (pt_x, pt_y, pt_x+width, pt_y+height)
+        self.objects = list([])
+
+    def add_object(self, obj: GenericObject):
+        self.objects.append(obj)
+
+
+# In[2]:
+
+
+categories = {0: 'CARGO_PLANE', 1: 'HELICOPTER', 2: 'SMALL_CAR', 3: 'BUS', 4: 'TRUCK', 5: 'MOTORBOAT', 6: 'FISHING_VESSEL', 7: 'DUMP_TRUCK', 8: 'EXCAVATOR', 9: 'BUILDING', 10: 'STORAGE_TANK', 11: 'SHIPPING_CONTAINER'}
+
+
+# In[3]:
+
+
+import warnings
+import rasterio
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
+
+# Define data augmentation parameters for helicopter images
+
+
+
+
+def load_geoimage(filename):
+    warnings.filterwarnings('ignore', category=rasterio.errors.NotGeoreferencedWarning)
+    src_raster = rasterio.open(filename, 'r')
+    # RasterIO to OpenCV (see inconsistencies between libjpeg and libjpeg-turbo)
+    input_type = src_raster.profile['dtype']
+    input_channels = src_raster.count
+    img = np.zeros((src_raster.height, src_raster.width, src_raster.count), dtype=input_type)
+    for band in range(input_channels):
+        img[:, :, band] = src_raster.read(band+1)
+    return img
+
+def generator_images(objs, batch_size, do_shuffle=False):
+    while True:
+        if do_shuffle:
+            np.random.shuffle(objs)
+        groups = [objs[i:i+batch_size] for i in range(0, len(objs), batch_size)]
+        for group in groups:
+            images, labels = [], []
+            for (filename, obj) in group:
+                # Load image
+                images.append(load_geoimage(filename))
+                probabilities = np.zeros(len(categories))
+                probabilities[list(categories.values()).index(obj.category)] = 1
+                labels.append(probabilities)
+            images = np.array(images).astype(np.float32)
+            labels = np.array(labels).astype(np.float32)
+            yield images, labels  
+            del images
+            del labels 
+
+# In[4]:
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+def draw_confusion_matrix(cm, categories):
+    # Draw confusion matrix
+    fig = plt.figure(figsize=[6.4*pow(len(categories), 0.5), 4.8*pow(len(categories), 0.5)])
+    ax = fig.add_subplot(111)
+    cm = cm.astype('float') / np.maximum(cm.sum(axis=1)[:, np.newaxis], np.finfo(np.float64).eps)
+    im = ax.imshow(cm, interpolation='nearest', cmap=plt.cm.get_cmap('Blues'))
+    ax.figure.colorbar(im, ax=ax)
+    ax.set(xticks=np.arange(cm.shape[1]), yticks=np.arange(cm.shape[0]), xticklabels=list(categories.values()), yticklabels=list(categories.values()), ylabel='Annotation', xlabel='Prediction')
+    # Rotate the tick labels and set their alignment
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
+    # Loop over data dimensions and create text annotations
+    thresh = cm.max() / 2.0
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], '.2f'), ha="center", va="center", color="white" if cm[i, j] > thresh else "black", fontsize=int(20-pow(len(categories), 0.5)))
+    fig.tight_layout()
+    # plt.show(fig)
+
+
+# #### Training
+# Design and train a ffNN to deal with the â??xview_recognitionâ? classification task.
+
+# In[5]:
+
+
+import json
+
+# Load database
+json_file = 'xview_recognition/xview_ann_train.json'
+with open(json_file) as ifs:
+    json_data = json.load(ifs)
+ifs.close()
+
+
+# In[7]:
+
+
+import numpy as np
+import os
+from keras.preprocessing.image import ImageDataGenerator
+import random
+
+
+counts = dict.fromkeys(categories.values(), 0)
+anns = []
+for json_img, json_ann in zip(json_data['images'], json_data['annotations']):
+    
+    image = GenericImage('xview_recognition/' + json_img['file_name'])
+    image.tile = np.array([0, 0, json_img['width'], json_img['height']])
+    obj = GenericObject()
+    obj.id = json_ann['id']
+    obj.bb = (int(json_ann['bbox'][0]), int(json_ann['bbox'][1]), int(json_ann['bbox'][2]), int(json_ann['bbox'][3]))
+    obj.category = list(categories.values())[json_ann['category_id']-1]
+    # Resampling strategy to reduce training time
+    counts[obj.category] += 1
+    image.add_object(obj)
+    anns.append(image)
+print(counts)
+
+# In[8]:
+
+
+from sklearn.model_selection import train_test_split
+
+anns_train, anns_valid = train_test_split(anns, test_size=0.1, random_state=1, shuffle=True)
+
+
+# In[9]:
+# Load architecture
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten, BatchNormalization
+from keras.layers import Conv2D, MaxPooling2D
+import keras
+import tensorflow as tf
+
+model_base = tf.keras.applications.ResNet50(
+    include_top=False,
+    weights= "imagenet",
+    input_tensor=None,
+    input_shape= (224, 224, 3),
+    pooling= max
+)
+
+for layer in model_base.layers:
+    layer.trainable = False
+
+model = Sequential()
+
+model.add(model_base)
+
+model.add(Conv2D(64, (3, 3), activation='gelu', kernel_initializer="he_normal"))
+model.add(BatchNormalization())
+model.add(Dropout(0.25))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+#model.add(Conv2D(128, (3, 3), activation='gelu', kernel_initializer="he_normal"))
+#model.add(BatchNormalization())
+#model.add(Dropout(0.25))
+#model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+
+model.add(Dense(100, activation = 'gelu', kernel_initializer = 'he_normal', kernel_regularizer=keras.regularizers.l1_l2(0.0001)))
+model.add(BatchNormalization())
+model.add(Dropout(rate = 0.2))
+
+
+model.add(Dense(len(categories), activation="softmax"))
+
+model.summary()
+
+
+# In[10]:
+
+
+from tensorflow.keras.optimizers import Adam, Adagrad, SGD
+"""
+optimizer = Adagrad(lr=1e-3, epsilon=1e-08, weight_decay=0.0001)
+model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+
+"""
+# Learning rate is changed to 0.001
+opt = Adam(learning_rate=1e-3, beta_1=0.9, beta_2=0.999, epsilon=1e-8, weight_decay=0.0001, amsgrad=True, clipnorm=1.0, clipvalue=0.5)
+model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+
+
+"""
+from tensorflow.keras.optimizers import RMSprop
+opt = RMSprop(lr=0.001, momentum=0.9, decay=0.0001)
+model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+"""
+"""
+optimizer = SGD(lr=0.01, momentum=0.9, decay=0.001)
+model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+"""
+# In[11]:
+
+
+from tensorflow.keras.callbacks import TerminateOnNaN, EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
+
+# Callbacks
+model_checkpoint = ModelCheckpoint('model.hdf5', monitor='val_accuracy', verbose=1, save_best_only=True)
+reduce_lr = ReduceLROnPlateau('val_accuracy', factor=0.1, patience=10, verbose=1)
+early_stop = EarlyStopping('val_accuracy', patience=40, verbose=1)
+terminate = TerminateOnNaN()
+callbacks = [model_checkpoint, reduce_lr, early_stop, terminate]
+
+
+# In[12]:
+
+
+# Generate the list of objects from annotations
+objs_train = [(ann.filename, obj) for ann in anns_train for obj in ann.objects]
+objs_valid = [(ann.filename, obj) for ann in anns_valid for obj in ann.objects]
+# Generators
+"""
+batch_size = 16
+"""
+batch_size = 20
+train_generator = generator_images(objs_train, batch_size, do_shuffle=True)
+valid_generator = generator_images(objs_valid, batch_size, do_shuffle=False)
+
+
+# In[13]:
+
+
+import math
+import numpy as np
+print('Training model')
+epochs = 1
+train_steps = math.ceil(len(objs_train)/batch_size)
+valid_steps = math.ceil(len(objs_valid)/batch_size)
+h = model.fit_generator(train_generator, steps_per_epoch=train_steps, validation_data=valid_generator, validation_steps=valid_steps, epochs=epochs, callbacks=callbacks, verbose=1)
+# Best validation model
+best_idx = int(np.argmax(h.history['val_accuracy']))
+best_value = np.max(h.history['val_accuracy'])
+print('Best validation model: epoch ' + str(best_idx+1), ' - val_accuracy ' + str(best_value))
+
+
+# #### Testing
+# Try to improve the results provided in the Moodle competition wiki.
+
+# In[14]:
+
+
+import json
+
+# Load database
+json_file = 'xview_recognition/xview_ann_test.json'
+with open(json_file) as ifs:
+    json_data = json.load(ifs)
+ifs.close()
+
+
+# In[15]:
+
+
+import numpy as np
+
+anns = []
+for json_img, json_ann in zip(json_data['images'], json_data['annotations']):
+    image = GenericImage('xview_recognition/' + json_img['file_name'])
+    image.tile = np.array([0, 0, json_img['width'], json_img['height']])
+    obj = GenericObject()
+    obj.id = json_ann['id']
+    obj.bb = (int(json_ann['bbox'][0]), int(json_ann['bbox'][1]), int(json_ann['bbox'][2]), int(json_ann['bbox'][3]))
+    obj.category = list(categories.values())[json_ann['category_id']-1]
+    image.add_object(obj)
+    
+
+anns.append(image)
+
+
+# In[16]:
+
+
+import numpy as np
+
+# model.load_weights('model.hdf5', by_name=True)
+y_true, y_pred = [], []
+for ann in anns:
+    # Load image
+    image = load_geoimage(ann.filename)
+    for obj_pred in ann.objects:
+        # Generate prediction
+        warped_image = np.expand_dims(image, 0)
+        predictions = model.predict(warped_image)
+        # Save prediction
+        pred_category = list(categories.values())[np.argmax(predictions)]
+        pred_score = np.max(predictions)
+        y_true.append(obj_pred.category)
+        y_pred.append(pred_category)
+
+
+# In[17]:
+
+
+from sklearn.metrics import confusion_matrix
+
+# Compute the confusion matrix
+cm = confusion_matrix(y_true, y_pred, labels=list(categories.values()))
+draw_confusion_matrix(cm, categories)
+
+
+# In[18]:
+
+
+import numpy as np
+
+# Compute the accuracy
+correct_samples_class = np.diag(cm).astype(float)
+total_samples_class = np.sum(cm, axis=1).astype(float)
+total_predicts_class = np.sum(cm, axis=0).astype(float)
+print('Mean Accuracy: %.3f%%' % (np.sum(correct_samples_class) / np.sum(total_samples_class) * 100))
+acc = correct_samples_class / np.maximum(total_samples_class, np.finfo(np.float64).eps)
+print('Mean Recall: %.3f%%' % (acc.mean() * 100))
+acc = correct_samples_class / np.maximum(total_predicts_class, np.finfo(np.float64).eps)
+print('Mean Precision: %.3f%%' % (acc.mean() * 100))
+for idx in range(len(categories)):
+    # True/False Positives (TP/FP) refer to the number of predicted positives that were correct/incorrect.
+    # True/False Negatives (TN/FN) refer to the number of predicted negatives that were correct/incorrect.
+    tp = cm[idx, idx]
+    fp = sum(cm[:, idx]) - tp
+    fn = sum(cm[idx, :]) - tp
+    tn = sum(np.delete(sum(cm) - cm[idx, :], idx))
+    # True Positive Rate: proportion of real positive cases that were correctly predicted as positive.
+    recall = tp / np.maximum(tp+fn, np.finfo(np.float64).eps)
+    # Precision: proportion of predicted positive cases that were truly real positives.
+    precision = tp / np.maximum(tp+fp, np.finfo(np.float64).eps)
+    # True Negative Rate: proportion of real negative cases that were correctly predicted as negative.
+    specificity = tn / np.maximum(tn+fp, np.finfo(np.float64).eps)
+    # Dice coefficient refers to two times the intersection of two sets divided by the sum of their areas.
+    # Dice = 2 |Aâ?ŠB| / (|A|+|B|) = 2 TP / (2 TP + FP + FN)
+    f1_score = 2 * ((precision * recall) / np.maximum(precision+recall, np.finfo(np.float64).eps))
+    print('> %s: Recall: %.3f%% Precision: %.3f%% Specificity: %.3f%% Dice: %.3f%%' % (list(categories.values())[idx], recall*100, precision*100, specificity*100, f1_score*100))
+
+
+
+
+
